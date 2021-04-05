7999d83d43573c9937232f812ecae2895a41e8f1
Jonas Gacrama <jonasgacrama@gmail.com>
2021-04-05 20:11:34 +0800

    Implemented the TextDelegate functions

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -21,17 +21,18 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+   
+    func textFieldDidEndEditing(_ textField: UITextField){
+        mealNameLabel.text = textField.text
+    }
     
     //MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        return false
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField){
-        
     }
 }
