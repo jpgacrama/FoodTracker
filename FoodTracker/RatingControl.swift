bafcddfccae197286ca6ae987c77a11febf0a7fc
Jonas Gacrama <jonasgacrama@gmail.com>
2021-04-07 19:24:28 +0800

    Finished creating the Buttons

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,4 +8,18 @@ class RatingControl: UIStackView {
     required init(coder: NSCoder) {
         super.init(coder: coder)
     }
+    
+    private func setupButtons() {
+        // Button Properties
+        let button = UIButton()
+        button.backgroundColor = UIColor.blue
+        
+        // Button constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
 }
