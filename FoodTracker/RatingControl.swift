59aa996b1389cd18cafe49793daa60755bd8ba5f
Jonas Gacrama <jonasgacrama@gmail.com>
2021-04-07 18:12:08 +0800

    Removed excess comments for the RatingControl class

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -1,20 +1,4 @@
-//
-//  RatingControl.swift
-//  FoodTracker
-//
-//  Created by Jonas Gacrama on 4/7/21.
-//
-
 import UIKit
 
 class RatingControl: UIStackView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
