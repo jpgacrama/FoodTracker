975fec7f5497f3fed71bfb219ff6d387b42efdec
Jonas Gacrama <jonasgacrama@gmail.com>
2021-04-07 18:19:15 +0800

    Added class initializers

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -1,4 +1,11 @@
 import UIKit
 
 class RatingControl: UIStackView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }
