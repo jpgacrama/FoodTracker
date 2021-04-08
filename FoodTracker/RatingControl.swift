8373a5ef1f5decc9c8db28525ae3f776c631a548
Jonas Gacrama <jonasgacrama@gmail.com>
2021-04-08 18:56:27 +0800

    Added listeners to the star size and star count

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -1,12 +1,20 @@
 import UIKit
 
-@IBDesignable class RatingControl: UIStackView {
+class RatingControl: UIStackView {
     
     //MARK: Properties
     private var ratingButtons = [UIButton]()
     private var rating = 0
-    @IBInspectable private let starSize: CGSize = CGSize(width: 44.0, height: 44.0)
-    @IBInspectable private let starCount: Int = 5
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,7 +31,14 @@ import UIKit
     }
     
     private func setupButtons() {
-        for _ in 0..<starCount {
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+
+        for _ in 0 ..< starCount {
             // Create the button
             let button = UIButton()
             button.backgroundColor = UIColor.blue
