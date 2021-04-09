eb90a0452363771bd0615229a5bf04a9222fa57f
Jonas Gacrama <jonasgacrama@gmail.com>
2021-04-09 14:38:34 +0800

    Created the MealTableViewCell class

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Jonas Gacrama on 4/9/21.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
