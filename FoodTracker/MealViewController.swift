bdf39406b1645ac0487c3051f6736155e106c038
Jonas Gacrama <jonasgacrama@gmail.com>
2021-04-09 20:54:49 +0800

    Added the prepare() function which sets the Meal class when the segue transitions

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import os.log
 
 class MealViewController: UIViewController, UITextFieldDelegate,
                       UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -38,6 +39,26 @@ class MealViewController: UIViewController, UITextFieldDelegate,
    
     func textFieldDidEndEditing(_ textField: UITextField){
         // TODO: Replace this soon
+    }
+ 
+    // MARK: Navigation
+
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let name = nameTextField.text ?? ""
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+        
+        // Set the meal to be passed to MealTableViewController after the unwind segue.
+        meal = Meal(name: name, photo: photo, rating: rating)
     }
     
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
