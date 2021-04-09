bf50706ad8124ff0daaf6b700a8443688abdf087
Jonas Gacrama <jonasgacrama@gmail.com>
2021-04-09 16:11:32 +0800

    Renamed ViewController to MealViewController
    This makes it more meaningful

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Jonas Gacrama on 4/4/21.
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate,
+class MealViewController: UIViewController, UITextFieldDelegate,
                       UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -31,7 +30,7 @@ class ViewController: UIViewController, UITextFieldDelegate,
     }
    
     func textFieldDidEndEditing(_ textField: UITextField){
-        mealNameLabel.text = textField.text
+        // TODO: Replace this soon
     }
     
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
