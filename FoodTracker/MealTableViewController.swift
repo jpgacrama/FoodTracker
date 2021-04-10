86dd2193bb6252e043ad1940cff7c059032de194
Jonas Gacrama <jonasgacrama@gmail.com>
2021-04-10 16:35:17 +0800

    I cannot find a one to one substitute for the deprecated unarchiveObject() function

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -161,4 +161,8 @@ class MealTableViewController: UITableViewController {
             os_log("Failed to save meals...", log: OSLog.default, type: .error)
         }
     }
+    
+    private func loadMeals() -> [Meal]? {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
+    }
 }
